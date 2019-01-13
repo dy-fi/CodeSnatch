954cfde039180e99b9334948940fafc22f980ce0
Dylan Finn <dylanfinn@Dylans-MacBook-Pro.local>
2019-01-12 17:02:20 -0800

    UI changes

---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#preview').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#file").change(function(){
+    readURL(this);
+});
